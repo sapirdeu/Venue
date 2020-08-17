d436b1a6e93bec32bfbebcf8da4a6661ef0ea791
sapirdeu <sapiroosh.d@gmail.com>
2020-08-18 00:00:09 +0300

    finished "Venue info" section

--- venue/src/App.js
+++ venue/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Featured from './components/featured/Featured';
+import VenueNfo from './components/venueNfo/VenueNfo';
 
 function App() {
   return (
     <div className="App" style={{ height:"1500px",background:'cornflowerblue'}}>
       <Header/>
       <Featured/>
+      <VenueNfo/>
     </div>
   );
 }
